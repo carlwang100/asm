d7c57255baeaa582370e90caf842c6ac92a72cf2
xiaoyaowang <xiaoyaowang@douyu.tv>
2020-12-29 21:50:32 +0800

    demo

--- WC-compile/src/main/java/wc/learning/plugin/WcPlugin.java
+++ WC-compile/src/main/java/wc/learning/plugin/WcPlugin.java
@@ -1,12 +1,16 @@
 package wc.learning.plugin;
 
+import com.android.build.gradle.AppExtension;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 public class WcPlugin implements Plugin<Project> {
 
     @Override
-    public void apply(Project o) {
-        System.out.println("i am best--------->>>>>>");
+    public void apply(Project project) {
+        System.out.println("i am 1.0.3---------");
+        AppExtension android = project.getExtensions().getByType(AppExtension.class);
+        android.registerTransform(new WcTransform(project));
     }
 }
